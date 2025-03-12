3bcdbeb5e323709670e58a216f90eb57e1417a86
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-03-11 23:28:45 -0500

    cambiossss

--- medicineapp/core/urls.py
+++ medicineapp/core/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
-from django.urls import path, include
-from core.views import index 
+from django.urls import path
+from .views import index, upload_image
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),  
-    path('upload/', include('core.urls')),  
+    path('', index, name='index'),
+    path('upload/', upload_image, name='upload_image'),
 ]
 
 
