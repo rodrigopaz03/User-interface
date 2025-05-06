c15fda399eda80bb10dda0b8a5cf06bcea7c4fd2
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-06 18:12:04 -0500

    -

--- medicineapp/medicineapp/urls.py
+++ medicineapp/medicineapp/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Incluye las URLs definidas en core
-    path('api/', include('core.urls')),
 ]
