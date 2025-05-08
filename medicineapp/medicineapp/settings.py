cef9ccbedcce18677c863b27d0ebf4caad907cb6
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-08 18:40:37 -0500

    conexion servidor

--- medicineapp/medicineapp/settings.py
+++ medicineapp/medicineapp/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 SERVER1_URL = 'http://34.8.244.158:8080/upload/'
-SERVER2_URL = 'http://34.31.227.208:8080/registro/'
+SERVER2_URL = 'http://34.173.186.95:8080/registro/'
 
 
 
