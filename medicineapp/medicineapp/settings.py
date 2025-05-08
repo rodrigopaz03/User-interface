5d5bef87ba366cf2e174e04c8866a4b46844c6b0
bsalcedosuarez <b.salcedo@uniandes.edu.co>
2025-05-08 07:37:08 -0500

    ip

--- medicineapp/medicineapp/settings.py
+++ medicineapp/medicineapp/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-SERVER1_URL = 'http://34.31.227.208:8080/upload/'
-SERVER2_URL = 'http://34.31.212.91:8080/registro/'
+SERVER1_URL = 'http://35.225.154.203:8080/upload/'
+SERVER2_URL = 'http://34.171.216.141:8080/registro/'
 
 # Application definition
 INSTALLED_APPS = [
