f5ea7bcb061a4007ce71b10c8577963137e385ff
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-08 13:58:32 -0500

    urls

--- medicineapp/medicineapp/urls.py
+++ medicineapp/medicineapp/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Incluye las URLs definidas en core
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'', include('social_django.urls')),
 ]
