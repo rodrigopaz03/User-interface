04ad676a4ea2d382b9b944db50f4d42a7cbfe97d
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-09 06:39:20 -0500

    cambios

--- medicineapp/medicineapp/settings.py
+++ medicineapp/medicineapp/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 SERVER1_URL = 'http://34.173.186.95:8080/upload/'
-SERVER2_URL = 'http://34.171.216.141:8080/registro/'
+SERVER2_URL = 'http://34.173.186.95:8080/registro/'
 
 # Application definition
 INSTALLED_APPS = [
