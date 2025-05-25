aba9d3164b3bf4f2861ff5c6612c449c9e44cc3b
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-24 22:04:22 -0500

    nueva ip server 1

--- medicineapp/medicineapp/settings.py
+++ medicineapp/medicineapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-SERVER1_URL = 'http://34.8.244.158:8080/upload/'
+SERVER1_URL = 'http://34.46.253.193:8080/upload/'
 SERVER2_URL = 'http://34.69.130.44:8080/registro/'
 
 
